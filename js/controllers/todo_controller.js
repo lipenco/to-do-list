c8047d944a677a0e76fc637853e4b2f93ab67ef4
Magdalena Lipinska <mag.lipinska@gmail.com>
2013-09-27 20:06:01 +0900

    accepting editing

--- js/controllers/todo_controller.js
+++ js/controllers/todo_controller.js
@@ -11,5 +11,16 @@ Todos.TodoController = Ember.ObjectController.extend({
       model.save();
       return value;
     }
-  }.property('model.isCompleted')
+  }.property('model.isCompleted'),
+
+actions: {
+   editTodo: function () {
+     this.set('isEditing', true);
+   },
+   acceptChanges: function () {
+     this.set('isEditing', false);
+     this.get('model').save();
+   }
+}
+
 });
